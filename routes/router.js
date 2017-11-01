48854a94cd962b452542b6d42eff3b02778c1731
yitsol <yitsol>
2017-11-01 18:24:59 +0530

    01commit

---
+++ routes/router.js
@@ -0,0 +1,116 @@
+var express = require('express');
+var router = express.Router();
+var User = require('../models/user');
+
+
+// GET route for reading data
+router.get('/', function (req, res, next) {
+  return res.sendFile(path.join(__dirname + '/templateLogReg/index.html'));
+});
+
+
+//POST route for updating data
+router.post('/', function (req, res, next) {
+  // confirm that user typed same password twice
+  if (req.body.password !== req.body.passwordConf) {
+    var err = new Error('Passwords do not match.');
+    err.status = 400;
+    res.send("passwords dont match");
+    return next(err);
+  }
+
+  if (req.body.email &&
+    req.body.username &&
+    req.body.password &&
+    req.body.passwordConf) {
+
+    var userData = {
+      email: req.body.email,
+      username: req.body.username,
+      password: req.body.password,
+      passwordConf: req.body.passwordConf,
+    }
+
+    User.create(userData, function (error, user) {
+      if (error) {
+        return next(error);
+      } else {
+        req.session.userId = user._id;
+        return res.redirect('/profile');
+      }
+    });
+
+  } else if (req.body.logemail && req.body.logpassword) {
+    User.authenticate(req.body.logemail, req.body.logpassword, function (error, user) {
+      if (error || !user) {
+        var err = new Error('Wrong email or password.');
+        err.status = 401;
+        return next(err);
+      } else {
+        req.session.userId = user._id;
+        return res.redirect('/profile');
+      }
+    });
+  } else {
+    var err = new Error('All fields required.');
+    err.status = 400;
+    return next(err);
+  }
+})
+
+// GET route after registering
+router.get('/profile', function (req, res, next) {
+  User.findById(req.session.userId)
+    .exec(function (error, user) {
+      if (error) {
+        return next(error);
+      } else {
+        if (user === null) {
+          var err = new Error('Not authorized! Go back!');
+          err.status = 400;
+          return next(err);
+        } else {
+          return res.send('<h1>Name: </h1>' + user.username 
+                        + '<h2>Mail: </h2>' + user.email 
+                        + '<br><a type="button" href="/logout">Logout</a>');
+        }
+      }
+    });
+});
+
+
+// GET list of users
+router.get('/listUsers', function (req, res, next) {
+  User.find({}).exec(function (error, users) {
+      if (error) {
+        return next(error);
+      }else{
+        //console.log('-----'+JSON.stringify(users));
+        return res.send(users);
+        /*var rs = '<h1>Users List </h1><br/>';
+        var i = 0;
+        while(users.length>i){
+          rs = rs + '<h4>'+users[i].email+'</h4>-+-<h4>'+users[i].username+'</h4>';
+          i++;
+        }
+        res.send(rs);*/
+      }
+    });
+});
+
+
+// GET for logout logout
+router.get('/logout', function (req, res, next) {
+  if (req.session) {
+    // delete session object
+    req.session.destroy(function (err) {
+      if (err) {
+        return next(err);
+      } else {
+        return res.redirect('/');
+      }
+    });
+  }
+});
+
+module.exports = router;
